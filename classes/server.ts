929b3b7f027f347776163a14a42d856ae9374c5f
holger <holger@localhost.localdomain>
2018-12-15 10:47:26 -0500

    fase 2

--- classes/server.ts
+++ classes/server.ts
@@ -1,17 +1,52 @@
 import {SERVER_PORT} from "../global/environment";
 import express from 'express';
+import socketIO from 'socket.io';
+import http from 'http';
+import * as socket from '../sockets/socket'
 
 export class Server {
+    private static _instance: Server;
+
     public app: express.Application;
     public port: number;
 
-    constructor() {
+    public io: socketIO.Server;
+    private httpServer: http.Server;
+
+    private constructor() {
         this.app = express();
         this.port = SERVER_PORT;
+
+        this.httpServer = new http.Server(this.app);
+        this.io = socketIO(this.httpServer);
+
+        this.escucharSockets();
     }
 
+    public static get instance() {
+        return this._instance || (this._instance = new this())
+    }
+
+    private escucharSockets() {
+        console.log("Escuchando conexiones - sockets");
+        this.io.on('connection', cliente => {
+            console.log("Cliente conectado");
+
+            /*
+            cliente.on('disconnect', () => {
+                console.log("Cliente desconectado");
+            });
+            */
+            //Estate pendiente del evento mensaje
+            socket.mensaje(cliente, this.io);
+
+            socket.desconectar(cliente);
+        });
+    }
+
+
     start(callaback: Function) {
-        this.app.listen(this.port, callaback);
+        this.httpServer.listen(this.port, callaback);
 
     }
 }
